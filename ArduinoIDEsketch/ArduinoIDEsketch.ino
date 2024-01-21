64dca0b96ee818b1d56b607ba9dda48dbe14f765
vasing <mail@tislenko.de>
2024-01-21 18:30:09 +0100

    ESP8266 ISR fix

--- ArduinoIDEsketch/ArduinoIDEsketch.ino
+++ ArduinoIDEsketch/ArduinoIDEsketch.ino
@@ -1,16 +1,20 @@
+void ICACHE_RAM_ATTR ISR();
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(16, INPUT_PULLDOWN_16); //D0
+  pinMode(5, INPUT); //D1
   pinMode(2, OUTPUT); //D4
-  attachInterrupt(digitalPinToInterrupt(16), ISR, RISING);
-
+  attachInterrupt(digitalPinToInterrupt(5), ISR, CHANGE);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+    delay(1000);
+    Serial.println("loop");
 }
 
-void ISR() {
+void ICACHE_RAM_ATTR ISR() {
  digitalWrite(2, !digitalRead(2));
+  Serial.println("ISR");
 }
