03edfb2233b0121390e7bcd562d9d03c93d84e74
vasing <mail@tislenko.de>
2024-02-06 13:18:18 +0300

    PWM fan control

--- ArduinoIDEsketch/ArduinoIDEsketch.ino
+++ ArduinoIDEsketch/ArduinoIDEsketch.ino
@@ -1,17 +1,41 @@
+#define outPIN 2
+#define InterruptPIN 5
+#define pwmPIN 15
+
+int incomingByte = 0; // serial incomming data
+int dutyCycle = 100; // PWM dutyCycle initial
+
 void ICACHE_RAM_ATTR ISR();
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(5, INPUT); //D1
-  pinMode(2, OUTPUT); //D4
-  attachInterrupt(digitalPinToInterrupt(5), ISR, CHANGE);
+  pinMode(InterruptPIN, INPUT); //D1
+  pinMode(outPIN, OUTPUT); //D4
+  pinMode(pwmPIN, OUTPUT); //D8 PWM
+  attachInterrupt(digitalPinToInterrupt(InterruptPIN), ISR, CHANGE);
   Serial.begin(115200);
+
+  analogWriteFreq(25000); // set PWM frequency to 20kHz
+  analogWrite(pwmPIN, 128);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    // send data only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+    dutyCycle = dutyCycle;
     delay(1000);
-    Serial.println("loop");
+  }
+  
+  
+  analogWrite(0, dutyCycle);
+  Serial.println(incomingByte, DEC);
 }
 
 void ICACHE_RAM_ATTR ISR() {
